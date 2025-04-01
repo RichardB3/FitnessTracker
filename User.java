72a5211b00b5eb694d5f984369b12eacce8c1cba
KennyQiH <160ken.qq@gmail.com>
2025-04-01 18:08:43 +0100

    User class completed

--- User.java
+++ User.java
@@ -1,4 +1,34 @@
 import java.awt.*;
 
 public abstract class User {
+	  private String name;
+	  private int age;
+	  private double weight;
+	  private double height;
+	  
+	  public User(String name, int age, double weight, double height) {
+		  this.name = name;
+		  this.age = age;
+		  this.weight = weight;
+		  this.height = height;}
+	  
+	  /*Getters for the user attributes
+	   * if the variables were to be called
+	     elsewhere */
+	  public String getname() {
+		  return name;}
+	  
+	  public int getage() {
+		  return age;}
+	  
+	  public double weight() {
+		  return weight;}
+	  
+	  public double height() {
+		  return height;}
+	  
+	  //Override
+	  public String toString() {
+		  return name;
+	  }
 }
