83642caaa4687a5e2cb51a0bb5fee53298cc507d
Kenny Lee <160ken.qq@gmail.com>
2025-04-02 20:48:23 +0100

    Update FitnessGUI.java

--- FitnessGUI.java
+++ FitnessGUI.java
@@ -1,9 +1,10 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class FitnessGUI {
-
-  public static void main(String[] args) {
+public class FitnessGUI extends JFrame{
+  class FitnessGUI() {
+    public static void main(String[] args) {
     
+    }
   }
 }
