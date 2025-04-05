dbcd818436af9579262b70feefde09b1ad4906e3
Kenny Lee <160ken.qq@gmail.com>
2025-04-05 01:14:52 +0100

    Update FitnessGUI.java

--- FitnessGUI.java
+++ FitnessGUI.java
@@ -1,10 +1,24 @@
 import java.awt.*;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FitnessGUI extends JFrame{
-  class FitnessGUI() {
-    public static void main(String[] args) {
-    
-    }
+public class FitnessTracker {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(FitnessGUI::new);
+  }
+
+class FitnessGUI extends JFrame{
+  private CardLayout Layout;
+  private JPanel MainPanel;
+  
+  public FitnessGUI() {
+    setTitle("Smart Fitness Tracker");
+    setSize(600,400);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new BorderLayout());
+
+    setVisible(true);
   }
 }
