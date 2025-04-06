b7b06035777a123b3251e24431c90ceb919b55c9
Kenny Lee <160ken.qq@gmail.com>
2025-04-06 18:55:06 +0100

    Update Workout.java

--- Workout.java
+++ Workout.java
@@ -3,4 +3,11 @@ import java.swing.*;
 import java.util.ArrayList;
 
 public class Workout extends JPanel {
+  private JTextField
+  private JTextArea
+  private ArrayList<String> Workouts = new ArrayList<>();
+
+  public Workout() {
+    setLayout(new GridLayout(5,5));
+  }
 }
